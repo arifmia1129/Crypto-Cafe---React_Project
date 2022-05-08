6d7f944b8648ff4e7ea80b9f67e2f7a8b9cfb0ee
arifvtti <arif.vtti@gmail.com>
2022-05-08 12:11:56 +0600

    single coin details route added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import CoinDetail from './components/CoinDetail/CoinDetail';
 import Coins from './components/Coins/Coins';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/coins" element={<Coins />}></Route>
+        <Route path="/coins/:id" element={<CoinDetail />}></Route>
       </Routes>
       <Footer></Footer>
     </div>
