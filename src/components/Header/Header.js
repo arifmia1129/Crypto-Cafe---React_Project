428efe5f75c362afb956cb5ace10f784fb1f2a37
arifvtti <arif.vtti@gmail.com>
2022-05-08 13:19:14 +0600

    customlink added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
+
 
 const Header = () => {
     return (
         <div className='sticky top-0 flex font-sans px-12 py-8 bg-white text-xl font-semibold justify-center md:justify-between'>
             <div className='hidden md:block font-bold text-1xl text-gray-600'>
-                <Link to="/">Crypto Cafe</Link>
+                <CustomLink to="/">Crypto Cafe</CustomLink>
             </div>
             <div className='gap-4 flex'>
-                <Link to="/">Home</Link>
-                <Link to="/coins">Coins</Link>
-                <Link to="/contact">Contact</Link>
-                <Link to="/about">About</Link>
+                <CustomLink to="/">Home</CustomLink>
+                <CustomLink to="/coins">Coins</CustomLink>
+                <CustomLink to="/contact">Contact</CustomLink>
+                <CustomLink to="/about">About</CustomLink>
             </div>
         </div>
     );
