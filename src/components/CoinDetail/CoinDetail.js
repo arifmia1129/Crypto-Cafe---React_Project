74710cca746d141fc4436a475b978f3d9305397e
arifvtti <arif.vtti@gmail.com>
2022-05-08 13:45:26 +0600

    spinner added

--- src/components/CoinDetail/CoinDetail.js
+++ src/components/CoinDetail/CoinDetail.js
@@ -1,48 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Spinner from '../Spinner/Spinner';
 
 const CoinDetail = () => {
     const { id } = useParams();
     const [coin, setCoin] = useState({});
+    const [status, setStatus] = useState(false);
     useEffect(() => {
+        setStatus(true);
         fetch(`https://api.coingecko.com/api/v3/coins/${id}`)
             .then(res => res.json())
-            .then(data => setCoin(data));
+            .then(data => {
+                setStatus(false)
+                setCoin(data)
+            });
     }, [id]);
     const { image, name, market_cap_rank, country_origin, hashing_algorithm, genesis_date, last_updated, coingecko_score, community_score, developer_score, liquidity_score, public_interest_score } = coin;
     return (
-        <div className='md:px-80 py-10 px-[8%] md:flex'>
-            <div className='py-5 md:order-2 flex justify-center items-center md:px-40'>
-                <img src={image?.large} alt="" />
-            </div>
-            <div className='md:order-1 font-sans'>
-                <div className='mb-20'>
-                    <h3 className='text-3xl font-bold'>General Info</h3>
-                    <div className='text-xl'>
-                        <p>Name: {name}</p>
-                        <p>Market Cap Rank: {market_cap_rank}</p>
-                        <p>Country Origin: {country_origin ? country_origin : "Not available"}</p>
-                        <p>Hashing Algorithm: {hashing_algorithm}</p>
-                        <p>Genesis Data: {genesis_date}</p>
-                        <p>Last Update: {last_updated}</p>
+        <>
+            {
+                status ? <Spinner /> : <div className='md:px-80 py-10 px-[8%] md:flex'>
+                    <div className='py-5 md:order-2 flex justify-center items-center md:px-40'>
+                        <img src={image?.large} alt="" />
+                    </div>
+                    <div className='md:order-1 font-sans'>
+                        <div className='mb-20'>
+                            <h3 className='text-3xl font-bold'>General Info</h3>
+                            <div className='text-xl'>
+                                <p>Name: {name}</p>
+                                <p>Market Cap Rank: {market_cap_rank}</p>
+                                <p>Country Origin: {country_origin ? country_origin : "Not available"}</p>
+                                <p>Hashing Algorithm: {hashing_algorithm}</p>
+                                <p>Genesis Data: {genesis_date}</p>
+                                <p>Last Update: {last_updated}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <h3 className='text-3xl font-bold'>Scores Info</h3>
+                            <div className='text-xl'>
+                                <p>Coingecko Score: {coingecko_score}</p>
+                                <p>Community Score: {community_score}</p>
+                                <p>Developer Score: {developer_score}</p>
+                                <p>Liquidity Score: {liquidity_score}</p>
+                                <p>Public Interest Score: {public_interest_score}</p>
+                            </div>
+                        </div>
+
+                        <div>
+
+                        </div>
                     </div>
                 </div>
-                <div>
-                    <h3 className='text-3xl font-bold'>Scores Info</h3>
-                    <div className='text-xl'>
-                        <p>Coingecko Score: {coingecko_score}</p>
-                        <p>Community Score: {community_score}</p>
-                        <p>Developer Score: {developer_score}</p>
-                        <p>Liquidity Score: {liquidity_score}</p>
-                        <p>Public Interest Score: {public_interest_score}</p>
-                    </div>
-                </div>
-
-                <div>
-
-                </div>
-            </div>
-        </div>
+            }
+        </>
     );
 };
 
--- src/components/Coins/Coins.js
+++ src/components/Coins/Coins.js
@@ -1,26 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import Coin from '../Coin/Coin';
+import Spinner from '../Spinner/Spinner';
 
 const Coins = () => {
     const [coins, setCoins] = useState([]);
+    const [status, setStatus] = useState(false);
     useEffect(() => {
+        setStatus(true);
         fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false")
             .then(res => res.json())
-            .then(data => setCoins(data));
+            .then(data => {
+                setStatus(false)
+                setCoins(data)
+            });
     }, []);
     return (
-        <div className='px-[8%] md:px-40 py-8'>
-            <h1 className='font-sans font-bold text-4xl text-center'>Available Crypto Currencies</h1>
-            <p className='text-center py-3 text-xl text-gray-600'>Total Coins: {coins.length}</p>
-            <div className='md:grid grid-cols-4 gap-4'>
-                {
-                    coins.map(coin => <Coin
-                        key={coin.id}
-                        coin={coin}
-                    ></Coin>)
-                }
-            </div>
-        </div>
+        <>
+            {
+                status ? <Spinner /> : <div className='px-[8%] md:px-40 py-8'>
+                    <h1 className='font-sans font-bold text-4xl text-center'>Available Crypto Currencies</h1>
+                    <p className='text-center py-3 text-xl text-gray-600'>Total Coins: {coins.length}</p>
+                    <div className='md:grid grid-cols-4 gap-4'>
+                        {
+                            coins.map(coin => <Coin
+                                key={coin.id}
+                                coin={coin}
+                            ></Coin>)
+                        }
+                    </div>
+                </div>
+            }
+        </>
     );
 };
 
